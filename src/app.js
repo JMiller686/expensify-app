c8f5bb2a87d4bb7d31c022364a59e1bd901e8e97
JMiller686 <jmiller686@gmail.com>
2019-03-28 10:39:08 -0500

    installed and setup react-redux

--- src/app.js
+++ src/app.js
@@ -1,46 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-})
 
 
-// addExpense -> Water Bill
-// addExpense -> Gas Bill
-// setTextFilter -> bill
-// getVisibleExpenses -> print visible ones to the screen
-store.dispatch(addExpense({
-    description: 'Water bill',
-    amount: 12000,
-    createdAt: 1000
-}));
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(addExpense({ description: 'rent' }));
+store.dispatch(setTextFilter());
 
-store.dispatch(addExpense({
-    description: 'Gas bill',
-    amount: 15000,
-    createdAt: 1005
-}));
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
-store.dispatch(addExpense({
-    description: 'rent',
-    amount: 15000,
-    createdAt: 1005
-}));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
 
-store.dispatch(setTextFilter('water'));
-
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     // entry: './src/app.js',
-    entry: './src/playground/higherOrderComponent.js',
+    entry: './src/app.js',
     output: {
         path: path.join(__dirname, '/public'),
         filename: 'bundle.js'
