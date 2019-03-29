e0a80dae2b30faf325d7f573f7b4a00b13969951
JMiller686 <jmiller686@gmail.com>
2019-03-29 15:32:14 -0500

    added test for add expense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,37 @@
 import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import uuid from 'uuid';
+
+test('should setup add expense action object', () => {
+    const expenseData = {
+        description: 'rent',
+        amount: 109500,
+        createdAt: 5000,
+        note: 'This was last months rent'
+        
+    };
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    })
+})
+
+test('should setup add expense action object with defaults', () => {
+    const action = addExpense();
+    expect(action).toEqual({
+        type: "ADD_EXPENSE",
+        expense: {
+            description: '',
+            note: '',
+            amount: 0,
+            createdAt: 0,
+            id: expect.any(String)
+        }
+    })
+})
 
 test('should remove expense', () => {
     const action = removeExpense({id: '2345234fgeww45g'});
