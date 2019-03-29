245f952de4a9029460a747d349291228317e36e0
JMiller686 <jmiller686@gmail.com>
2019-03-29 15:19:11 -0500

    added test for edit expense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -6,4 +6,16 @@ test('should remove expense', () => {
         type: 'REMOVE_EXPENSE',
         id: '2345234fgeww45g'
     })
+});
+
+test('should setup edit expense action object', () => {
+    const action = editExpense('2345234fgeww45g', {description: 'cable', amount: '500'});
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '2345234fgeww45g',
+        updates: {
+            description: 'cable', 
+            amount: '500'
+        }
+    })
 })
