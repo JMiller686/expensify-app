d6589920f56c9f3a7fe05d7cb9c28714709a1c26
JMiller686 <jmiller686@gmail.com>
2019-04-03 13:23:29 -0500

    added test for expense filters with no data

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { DateRangePicker } from 'react-dates';
 import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../actions/filters';
 
-class ExpenseListFilters extends React.Component {
+export class ExpenseListFilters extends React.Component {
     state = {
         calendarFocused: null
     };
@@ -17,20 +17,27 @@ class ExpenseListFilters extends React.Component {
         }))
     }
     onTextChange = (e) => {
-        this.props.setTextFilter(e.target.value)
+        this.props.setTextFilter(e.target.value);
     }
     onSortChange = (e) => {
         if(e.target.value === 'amount') {
-            this.props.sortByAmount()
+            this.props.sortByAmount();
         } else if(e.target.value === 'date') {
-            this.props.sortByDate()
+            this.props.sortByDate();
         }
     }
     render() {
         return (
             <div>
-                <input type="text" value={this.props.filters.text} onChange={this.onTextChange}/>
-                <select value={this.props.filters.sortBy} onChange={this.onSortChange}>
+                <input 
+                    type="text" 
+                    value={this.props.filters.text} 
+                    onChange={this.onTextChange}
+                />
+                <select 
+                    value={this.props.filters.sortBy} 
+                    onChange={this.onSortChange}
+                >
                     <option value="date">Date</option>
                     <option value="amount">Amount</option>
                 </select>
