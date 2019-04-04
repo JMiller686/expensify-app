55a7509942451f6ac2efa21ba028a61419c6a845
JMiller686 <jmiller686@gmail.com>
2019-04-04 11:47:04 -0500

    added express.js and setup express server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = new express();
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('server is listening on port 3000')
+});
