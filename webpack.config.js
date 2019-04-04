892e02ec2aad4341c1b77035a42e669b31d13d30
JMiller686 <jmiller686@gmail.com>
2019-04-04 10:57:04 -0500

    added extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = (env) => {
     const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
     console.log('env', env);
     return {
         // entry: './src/app.js',
@@ -17,12 +19,12 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-    
+                use: CSSExtract.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
             }]
         },
         devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
