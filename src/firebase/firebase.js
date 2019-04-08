5e99a0e6cb611815c360627b67093c911fbf9b70
JMiller686 <jmiller686@gmail.com>
2019-04-08 15:50:04 -0500

    error connecting to test db

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyAUm9h8t4tgLcQLMURr4xgzSMrQtrL60f4",
-    authDomain: "expensify-7cbea.firebaseapp.com",
-    databaseURL: "https://expensify-7cbea.firebaseio.com",
-    projectId: "expensify-7cbea",
-    storageBucket: "expensify-7cbea.appspot.com",
-    messagingSenderId: "29089722516"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
