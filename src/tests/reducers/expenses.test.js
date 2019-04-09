7b813023abd5df24b7f63e6c6587875e7d79b546
JMiller686 <jmiller686@gmail.com>
2019-04-09 14:25:49 -0500

    fetch expenses from firebase

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -57,7 +57,7 @@ test('should edit an expense', () => {
         amount: 195,
         createdAt: 0
     })
-})
+});
 
 test('should not edit an expense if expense is not found', () => {
     const action = {
@@ -70,4 +70,13 @@ test('should not edit an expense if expense is not found', () => {
 
     const state = expensesReducer(expenses, action);
     expect(state).toEqual(expenses)
-})
+});
+
+test('should set expenses', () => {
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[1]]
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[1]]);
+});
